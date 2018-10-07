7b313f8f59bf237d93942de7a582414967ca2093
rogicalnote <43873925+rogicalnote@users.noreply.github.com>
2018-10-07 14:59:59 +0900

    7まで

--- Paiza/paiza-7-05-1.py
+++ Paiza/paiza-7-05-1.py
@@ -1,4 +1,4 @@
-
+﻿
 '''
 演習課題「RPGの攻撃シーン」
 右のコードには、RPGの攻撃シーンを表示するプログラムで、teamのメンバーが順番にattack関数を呼び出します。さらに、teamのメンバーが攻撃した後に、敵の体力(enemy_hp)を表示するようになっています。敵の最初の体力は標準入力によって入力されます。
@@ -37,5 +37,5 @@ team = {"勇者" : 200, "戦士" : 150, "魔法使い" : 100}
 for person, power in team.items():
     attack(person)
     # 以下に、敵の体力を減少させるコードを書く
-    enemy_hp = power
+    enemy_hp = enemy_hp-power
     output_ememy_hp(enemy_hp)
